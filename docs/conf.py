7b7167b6c8e87379e9dbb4fcf8ffe2bc669d721f
Ricardas <ricardas.brazinskas@meetiqm.com>
2023-03-14 15:59:55 +0100

    Fix quantifier in tag whitelist regex
    Require the MAJOR and MINOR parts of semver tag to have at least one digit

--- docs/conf.py
+++ docs/conf.py
@@ -143,7 +143,7 @@ intersphinx_mapping = {
 }
 
 # -- Options for sphinx_multiversion --------------------------------------------------
-smv_tag_whitelist = r'^[0-9]*\.[0-9]*$'  # Allow tags that match format "X.Y"
+smv_tag_whitelist = r'^[0-9]+\.[0-9]+$'  # Allow tags that match format "X.Y"
 smv_branch_whitelist = '$-'  # Exclude all local branches in versions list by using unmatchable pattern
 smv_remote_whitelist = '$-'  # Exclude all remote branches in versions list by using unmatchable pattern
 smv_released_pattern = r'^refs/tags/.*$'  # Tags recognized as releases
